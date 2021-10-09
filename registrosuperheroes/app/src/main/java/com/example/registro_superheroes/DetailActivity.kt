f00330e541b5661a15f27a8900447081ea089c02
JavierGold <567498jav@gmail.com>
2021-10-09 12:40:49 -0500

    Recibir los datos del main en el detalle

--- registrosuperheroes/app/src/main/java/com/example/registro_superheroes/DetailActivity.kt
+++ registrosuperheroes/app/src/main/java/com/example/registro_superheroes/DetailActivity.kt
@@ -2,10 +2,24 @@ package com.example.registro_superheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.registro_superheroes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras!!
+        val superheroName = bundle.getString("superhero_name") ?: ""
+        val alterEgo = bundle.getString("alter_Ego") ?: ""
+        val bio = bundle.getString("bio") ?: ""
+        val power = bundle.getFloat("power")
+
+        binding.heroName.text = superheroName
+        binding.alterEgoText.text=alterEgo
+        binding.bioText.text=bio
+        binding.ratingBar.rating=power
+
     }
 }
