2d92eab433d69fc11ddd73bb55f4c81a0b494605
JavierGold <567498jav@gmail.com>
2021-10-09 11:58:34 -0500

    Abrir la actividad de detalle en el main

--- registrosuperheroes/app/src/main/java/com/example/registro_superheroes/MainActivity.kt
+++ registrosuperheroes/app/src/main/java/com/example/registro_superheroes/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.registro_superheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.registro_superheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener{
+
+        }
+    }
+
+    private fun openDetailActivity(){
+        val intent = Intent(this,DetailActivity::class.java)
+        startActivity(intent)
     }
 }
