01c587945c98d1b6734c64876197f649631bdb86
JavierGold <567498jav@gmail.com>
2021-10-11 17:09:16 -0500

    clase repository

--- earthquake/app/src/main/java/com/example/earthquake/MainViewModel.kt
+++ earthquake/app/src/main/java/com/example/earthquake/MainViewModel.kt
@@ -13,42 +13,11 @@ class MainViewModel: ViewModel() {
     val eqList: LiveData<MutableList<Earthquake>>
         get() = _eqList
 
+    private val repository = MainRepository()
+
     init {
         viewModelScope.launch {
-            _eqList.value = fetchEarthquakes()
+            _eqList.value = repository.fetchEarthquakes()
         }
     }
-
-    private suspend fun fetchEarthquakes(): MutableList<Earthquake> {
-        return withContext(Dispatchers.IO){
-            val eqJsonResponse = service.getLastHourEarthquakes()
-           val eqList =  parseEqResult(eqJsonResponse)
-            eqList
-
-        }
-    }
-
-    private fun parseEqResult(eqJsonResponse: EqJsonResponse): MutableList<Earthquake>{
-        val eqList = mutableListOf<Earthquake>()
-        val featureList = eqJsonResponse.features
-
-        for(feature in featureList){
-            val properties = feature.properties
-
-            val id = feature.id
-            val magnitude = properties.mag
-            val place = properties.place
-            val time = properties.time
-
-            val geometry = feature.geometry
-            val longitude = geometry.longitude
-            val latitude = geometry.latitude
-
-            eqList.add(Earthquake(id,place,magnitude,time,longitude,latitude))
-        }
-
-        return eqList
-    }
-
-
 }
